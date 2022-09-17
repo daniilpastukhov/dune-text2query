1e6b113d87447b82114b124195d2f2ba386b99f6
Daniil Pastukhov <pastudan@fit.cvut.cz>
2022-09-17 18:56:09 +0200

    Update

--- scraper/dune_scraper/db.py
+++ scraper/dune_scraper/db.py
@@ -9,10 +9,14 @@ load_dotenv()
 class MongoDatabase:
     def __init__(self) -> None:
         self.client = MongoClient(os.environ['MONGO_URI'])
-        self.collection = self.client['db']['queries']
-
+        # self.collection = self.client['db']['queries']
+        self.collection = self.client['db']['filtered']
+        
     def insert_one(self, doc):
         self.collection.insert_one(doc)
+
+    def update_label(self, query_id, label):
+        self.collection.update_one({'_id': query_id}, {'$set': {'query_label': label}})
 
     def get_total_queries(self):
         return self.collection.count_documents({})
